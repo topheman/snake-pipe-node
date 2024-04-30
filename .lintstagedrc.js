2e8bf97ff20ea1010605e7f9f5426789b0868b66
Christophe Rosset <tophe@topheman.com>
2024-04-30 21:56:18 +0200

    chore: refer to tsconfig.json when typecheck - needed for lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*.{js,ts,jsx,tsx,mjs,cjs}': 'npm run lint:fix',
-  '*.{ts,tsx}': 'npm run typecheck:lintstaged' // flags from tsconfig aren't picked up when files are passed
+  '*.{ts,tsx}': () => ['npm run typecheck']
 }
